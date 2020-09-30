f171fe2ce78158891e3b83e522b169da1430c1e6
ps2xu <ps2xu@163.com>
2020-09-30 13:29:06 +0800

    Update startphp.sh

--- startphp.sh
+++ startphp.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-echo "Rebilud get start php App"
-read -p "Please input Appname:" appname
-read -p "Please set your RAM(defaul:256):" ramsize
-if [ -z "$ramsize" ];then
-	ramsize=256
-fi
-ibmcloud target --cf
-ibmcloud cf push
+./ngweb -c c -format pb
 
